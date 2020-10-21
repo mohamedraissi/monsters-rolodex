e806b99ba109c31504a3c5207d3c00e76ae4a932
mohamedraissi <mohamedraissi10@gmail.com>
2020-10-21 15:48:44 +0200

    Updates

---
+++ precache-manifest.eabba56678a5334d45a7fdd946ee74de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e06ce85dd11fda4cf68a7e79a174721",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9378ba7f402b0ed853f4",
+    "url": "/monsters-rolodex/static/css/main.6d4e7a6c.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9378ba7f402b0ed853f4",
+    "url": "/monsters-rolodex/static/js/main.2ac2cc15.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
